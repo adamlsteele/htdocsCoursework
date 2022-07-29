7df571634bc9d4f19878624b1987529cbdd2926a
Adam Steele <SteeAdCO16@AshfieldSchool.onmicrosoft.com>
2022-07-29 10:50:31 +0100

    Styling link changes

--- include/header.php
+++ include/header.php
@@ -10,8 +10,8 @@ require "include/connection.php";
     <link rel="icon" href="img/mdb-favicon.ico" type="image/x-icon" />
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.2/css/all.css" />
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap"/>
-    <link rel="stylesheet" href="../css/mdb.min.css" />
-    <link rel="stylesheet" href="../css/custom.css" />
+    <link rel="stylesheet" href="css/mdb.min.css" />
+    <link rel="stylesheet" href="css/custom.css" />
 </head>
 
 <!--Navigation bar-->
