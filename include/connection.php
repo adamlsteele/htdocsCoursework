efc5186dbd287f992e87e769c7b60cdc59995943
Adam Steele <SteeAdCO16@AshfieldSchool.onmicrosoft.com>
2022-08-01 19:08:35 +0100

    Removal of prepared statements

--- include/connection.php
+++ include/connection.php
@@ -3,6 +3,7 @@ class Connection {
     //First calls when an object is made based on the class
     //Sets up a connection using a connection string
     private $connection;
+    private $query;
 
     public function __construct() {
         //Variables used to form a connection string
@@ -17,13 +18,37 @@ class Connection {
         }
     }
 
-    //Peforms GET queries returning the statement result
-    //Arguements include the statement query, prepared statement variable types and values
-    public function executeQuery($query, $paramTypes, ... $paramValues) {
-        $preparedStatement = $this->connection->prepare($query);
-        $preparedStatement->bind_param($paramTypes, ... $paramValues);
+    //Get account details from email
+    public function getUserByEmail(string $email, $accountType) {
+        $query = "SELECT * FROM ".$accountType." WHERE Email = '".$email."'";
+        return $this->connection->query($query);
+    }
+
+    //Get account details from id
+    public function getUserByID(int $id, $accountType) {
+        switch($accountType) {
+            case("student"):
+                $this->query = "SELECT * FROM ".$accountType." WHERE StudentID = ?";
+            case("teacher"):
+                $this->query = "SELECT * FROM ".$accountType." WHERE TeacherID = ?";
+        }
+
+        $preparedStatement = $this->connection->prepare($this->query);
+        $preparedStatement->bind_param("s", $email);
+
         $preparedStatement->execute();
         return $preparedStatement;
+    }
+
+    //Create an account of certain type for a user and then return the account details
+    public function createAccount($email, $username, $password, $accountType) {
+        $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+        $query = "INSERT INTO ".$accountType."(Email, Username, Password) VALUES('".$email."', '".$email."', '".$hashedPassword."')";
+
+        $this->connection->query($query);
+
+        //Return the created account details via a GET statement
+        return $this->getUserByEmail($email, $accountType);
     }
 }
 
