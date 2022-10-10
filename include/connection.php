81c056728e923e1b5c24e302d7cdcdeb2bad0d20
Adam Steele <96735995+adamlsteele@users.noreply.github.com>
2022-10-10 15:55:39 +0100

    Tidied and commented

--- include/connection.php
+++ include/connection.php
@@ -1,4 +1,9 @@
 <?php
+//#### Connection Class ####
+//All SQL queries are handled in this class
+//Global class that all other files have access too
+
+
 class Connection {
     //First calls when an object is made based on the class
     //Sets up a connection using a connection string
@@ -19,34 +24,20 @@ class Connection {
         }
     }
 
-    //General update query that does not return any data
+    //General query function that allows for a custom query to be passed through rather than a pre-defined one
     public function query(string $query) {
         return $this->connection->query($query);
     }
 
-    public function getTopicByID(int $id) {
-        $this->query = "SELECT * FROM `topic` WHERE `TopicID` =".$id;
-        return $this->connection->query($this->query);
-    }
-
-    public function getAssignmentByID(int $id) {
-        $this->query = "SELECT * FROM `assignment` WHERE `AssignmentID` =".$id;
-        return $this->connection->query($this->query);
-    }
-
-    public function getQuestionsByID(int $topicID) {
-        $this->query = "SELECT * FROM `question` WHERE `TopicID` =".$topicID;
-        return $this->connection->query($this->query);
-    }
-
-
-    //Get account details from email
+    //#### User Related Queries ####
+    //
+    //
+    //Fetches a user's account details via their email
     public function getUserByEmail(string $email, $accountType) {
         $this->query = "SELECT * FROM `".$accountType."` WHERE `Email` = '".$email."'";
         return $this->connection->query($this->query);
     }
-
-    //Get account details from id
+    //Fetches a user's account details via their ID
     public function getUserByID(int $id, $accountType) {
         if($accountType == "student") {
             $this->query = "SELECT * FROM `student` WHERE `StudentID` = ".$id;
@@ -56,26 +47,7 @@ class Connection {
 
         return $this->connection->query($this->query);
     }
-
-    //Get class details from id
-    public function getClassByID(int $id) {
-        $this->query = "SELECT * FROM `class` WHERE `ClassID` = ".$id;
-        return $this->connection->query($this->query);
-    }
-
-    //Get student details within a class
-    public function getStudentsByID(int $id) {
-        $this->query = "SELECT * FROM `student` WHERE `ClassID` = ".$id;
-        return $this->connection->query($this->query);
-    }
-
-    //Get class details from code
-    public function getClassByCode(string $code) {
-        $this->query = "SELECT * FROM `class` WHERE `ClassCode` = '".$code."'";
-        return $this->connection->query($this->query);
-    }
-
-    //Create an account of certain type for a user and then return the account details
+    //Create an account of a specific type using the specified parameters
     public function createAccount($email, $username, $password, $accountType) {
         $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
         $query = "INSERT INTO `".$accountType."`(Email, Username, Password) VALUES('".$email."', '".$email."', '".$hashedPassword."')";
@@ -86,66 +58,43 @@ class Connection {
         return $this->getUserByEmail($email, $accountType);
     }
 
-    public function getAssignmentsByClassID($id) {
-        $this->query = "SELECT `AssignmentID`, `Date`, topic.TopicName FROM `assignment` INNER JOIN `topic` on assignment.TopicID = topic.TopicID WHERE `ClassID` = ".$id." AND `Date` > CURRENT_DATE()";
-        $this->connection->query($this->query);
-        echo $this->connection->error;
+    //#### Class Related Queries ####
+    //
+    //
+    //Fetches a specific class via its ID
+    public function getClassByID(int $id) {
+        $this->query = "SELECT * FROM `class` WHERE `ClassID` = ".$id;
         return $this->connection->query($this->query);
     }
-
+    //Fetch all the students in a class via a class ID
+    public function getStudentsByID(int $id) {
+        $this->query = "SELECT * FROM `student` WHERE `ClassID` = ".$id;
+        return $this->connection->query($this->query);
+    }
+    //Fetch a class via its code
+    public function getClassByCode(string $code) {
+        $this->query = "SELECT * FROM `class` WHERE `ClassCode` = '".$code."'";
+        return $this->connection->query($this->query);
+    }
+    //Fetch the set of classes that a teacher owns
     public function getClassesByTeacherID($id) {
         $this->query = "SELECT * FROM `class` WHERE `TeacherID` = ".$id;
         return $this->connection->query($this->query);
     }
-
+    //Fetch a class via its name
     public function getClassByName($name) {
         $this->query = "SELECT * FROM `class` WHERE `Class Name` = '".$name."'";
         return $this->connection->query($this->query);
     }
-
+    //Create a class using specified parameters
     public function createClass($id, $name, $description, $colour, $code) {
         $this->query = "INSERT INTO `class`(`TeacherID`, `ClassName`, `ClassDescription`, `ClassCode`, `ClassColour`) VALUES(".$id.", '".$name."', '".$description."', '".$code."', '".$colour."')";
         echo $this->query;
         return $this->connection->query($this->query);
     }
-
-    public function getTopics() {
-        $this->query = "SELECT * FROM `topic`";
-        return $this->connection->query($this->query);
-    }
-
-    public function getQuestionByID($id) {
-        $this->query = "SELECT * FROM `question` WHERE `QuestionID` = ".$id;
-        return $this->connection->query($this->query);
-    }
-
-    public function createAssignment($class, $topic, $dueDate) {
-        $this->query = "INSERT INTO `assignment`(`ClassID`, `TopicID`, `Date`) VALUES(".$class.", ".$topic.", '".$dueDate."')";
-        return $this->connection->query($this->query);
-    }
-
-    public function getAssignmentResult($assignmentID, $studentID) {
-        $this->query = "SELECT * FROM `result` WHERE `AssignmentID` = ".$assignmentID." AND `StudentID` = ".$studentID;
-        return $this->connection->query($this->query);
-    }
-
-    public function getRecentTopics($id) {
-        $this->query = "SELECT * FROM result INNER JOIN topic ON result.TopicID = topic.TopicID WHERE StudentID = ".$id." ORDER BY DateCompleted ASC LIMIT 2";
-        return $this->connection->query($this->query);
-    }
-
-    public function getResultsbyID($id) {
-        $this->query = "SELECT * FROM result WHERE StudentID = ".$id;
-        return $this->connection->query($this->query);
-    }
-
-    public function getPastAssignmentsByID($id) {
-        $this->query = "SELECT AssignmentID, Date, topic.TopicName FROM assignment INNER JOIN topic on assignment.TopicID = topic.TopicID WHERE ClassID = ".$id." AND Date < CURRENT_DATE()";
-        return $this->connection->query($this->query);
-    }
-
+    //Delete a class via its ID
     public function deleteClass($id) {
-        //Update all the students so that they are no longer in a class
+        //Update all the students so that they are no longer in a class by setting their class ID to null
         $this->query = "SELECT * FROM `student` WHERE `ClassID` = ".$id;
         $students = $this->connection->query($this->query);
         foreach($students as $student) {
@@ -160,19 +109,90 @@ class Connection {
         return $this->connection->query($this->query);
     }
 
+    //#### Topic Related Queries ####
+    //
+    //
+    //Fetch all topics
+    public function getTopics() {
+        $this->query = "SELECT * FROM `topic`";
+        return $this->connection->query($this->query);
+    }
+    //Fetches a specific topic via its ID
+    public function getTopicByID(int $id) {
+        $this->query = "SELECT * FROM `topic` WHERE `TopicID` =".$id;
+        return $this->connection->query($this->query);
+    }
+    //Fetch the recently completed topics for a student
+    public function getRecentTopics($id) {
+        $this->query = "SELECT * FROM result INNER JOIN topic ON result.TopicID = topic.TopicID WHERE StudentID = ".$id." ORDER BY DateCompleted ASC LIMIT 2";
+        return $this->connection->query($this->query);
+    }
+
+    //#### Question Related Queries ####
+    //
+    //
+    //Fetch the details of a question via its ID
+    public function getQuestionByID($id) {
+        $this->query = "SELECT * FROM `question` WHERE `QuestionID` = ".$id;
+        return $this->connection->query($this->query);
+    }
+    //Fetches a set of questions for a topic via its ID
+    public function getQuestionsByID(int $topicID) {
+        $this->query = "SELECT * FROM `question` WHERE `TopicID` =".$topicID;
+        return $this->connection->query($this->query);
+    }
+
+    //#### Assignment Related Queries ####
+    //
+    //   
+    //Fetches a specific assignment via its ID
+    public function getAssignmentByID(int $id) {
+        $this->query = "SELECT * FROM `assignment` WHERE `AssignmentID` =".$id;
+        return $this->connection->query($this->query);
+    }
+    //Fetch the assignments for a class that are not past the due date via a class ID
+    public function getAssignmentsByClassID($id) {
+        $this->query = "SELECT `AssignmentID`, `Date`, topic.TopicName FROM `assignment` INNER JOIN `topic` on assignment.TopicID = topic.TopicID WHERE `ClassID` = ".$id." AND `Date` > CURRENT_DATE()";
+        $this->connection->query($this->query);
+        echo $this->connection->error;
+        return $this->connection->query($this->query);
+    }
+    //Create an assignment for a specific class with a topic and due date
+    public function createAssignment($class, $topic, $dueDate) {
+        $this->query = "INSERT INTO `assignment`(`ClassID`, `TopicID`, `Date`) VALUES(".$class.", ".$topic.", '".$dueDate."')";
+        return $this->connection->query($this->query);
+    } 
+    //Fetch the past assignments for a class
+    public function getPastAssignmentsByID($id) {
+        $this->query = "SELECT AssignmentID, Date, topic.TopicName FROM assignment INNER JOIN topic on assignment.TopicID = topic.TopicID WHERE ClassID = ".$id." AND Date < CURRENT_DATE()";
+        return $this->connection->query($this->query);
+    }
+
+    //#### Result Related Queries ####
+    //
+    //
+    //Fetch the result for a student via their ID
+    public function getResultsbyID($id) {
+        $this->query = "SELECT * FROM result WHERE StudentID = ".$id;
+        return $this->connection->query($this->query);
+    }
+    //Fetch the results for an assignment for a particular student
+    public function getAssignmentResult($assignmentID, $studentID) {
+        $this->query = "SELECT * FROM `result` WHERE `AssignmentID` = ".$assignmentID." AND `StudentID` = ".$studentID;
+        return $this->connection->query($this->query);
+    }  
+    //Add the result for a particular topic with question history
     public function addTopicResult($topicID, $studentID, $questionsAnswered, $questionsCorrect, $questionOne, $questionOneResult, $questionTwo, $questionTwoResult, $questionThree, $questionThreeResult, $questionFour, $questionFourResult, $questionFive, $questionFiveResult) {
         $this->query = "INSERT INTO result(TopicID, StudentID, QuestionsAnswered, QuestionsCorrect, QuestionOneID, QuestionOneAnswer, QuestionTwoID, QuestionTwoAnswer,  QuestionThreeID, QuestionThreeAnswer, QuestionFourID, QuestionFourAnswer, QuestionFiveID, QuestionFiveAnswer) 
         VALUES (".$topicID.", ".$studentID.", ".$questionsAnswered.", ".$questionsCorrect.", ".$questionOne.", '".$questionOneResult."', ".$questionTwo.", '".$questionTwoResult."', ".$questionThree.", '".$questionThreeResult."', ".$questionFour.", '".$questionFourResult."', ".$questionFive.", '".$questionFiveResult."')";
         return $this->connection->query($this->query);
     }
-
+    //Add the result for an assignment with question history that teachers can update
     public function addAssignmentResult($assignmentID, $topicID, $studentID, $questionsAnswered, $questionsCorrect, $questionOne, $questionOneResult, $questionTwo, $questionTwoResult, $questionThree, $questionThreeResult, $questionFour, $questionFourResult, $questionFive, $questionFiveResult) {
         $this->query = "INSERT INTO result(AssignmentID, TopicID, StudentID, QuestionsAnswered, QuestionsCorrect, QuestionOneID, QuestionOneAnswer, QuestionTwoID, QuestionTwoAnswer,  QuestionThreeID, QuestionThreeAnswer, QuestionFourID, QuestionFourAnswer, QuestionFiveID, QuestionFiveAnswer) 
         VALUES (".$assignmentID.", ".$topicID.", ".$studentID.", ".$questionsAnswered.", ".$questionsCorrect.", ".$questionOne.", '".$questionOneResult."', ".$questionTwo.", '".$questionTwoResult."', ".$questionThree.", '".$questionThreeResult."', ".$questionFour.", '".$questionFourResult."', ".$questionFive.", '".$questionFiveResult."')";
         $this->connection->query($this->query);
         return;
     }
-
 }
-
 ?>
