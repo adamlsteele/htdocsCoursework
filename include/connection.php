fd74d944f008413cbe22b920bf2d3b033a8f34d4
Adam Steele <96735995+adamlsteele@users.noreply.github.com>
2022-10-10 13:06:10 +0100

    Added backticks on SQL syntax

--- include/connection.php
+++ include/connection.php
@@ -25,33 +25,33 @@ class Connection {
     }
 
     public function getTopicByID(int $id) {
-        $this->query = "SELECT * FROM topic WHERE TopicID =".$id;
+        $this->query = "SELECT * FROM `topic` WHERE `TopicID` =".$id;
         return $this->connection->query($this->query);
     }
 
     public function getAssignmentByID(int $id) {
-        $this->query = "SELECT * FROM assignment WHERE AssignmentID =".$id;
+        $this->query = "SELECT * FROM `assignment` WHERE `AssignmentID` =".$id;
         return $this->connection->query($this->query);
     }
 
     public function getQuestionsByID(int $topicID) {
-        $this->query = "SELECT * FROM `question` WHERE TopicID =".$topicID;
+        $this->query = "SELECT * FROM `question` WHERE TopicID` =".$topicID;
         return $this->connection->query($this->query);
     }
 
 
     //Get account details from email
     public function getUserByEmail(string $email, $accountType) {
-        $this->query = "SELECT * FROM ".$accountType." WHERE Email = '".$email."'";
+        $this->query = "SELECT * FROM `".$accountType."` WHERE `Email` = '".$email."'";
         return $this->connection->query($this->query);
     }
 
     //Get account details from id
     public function getUserByID(int $id, $accountType) {
         if($accountType == "student") {
-            $this->query = "SELECT * FROM student WHERE StudentID = ".$id;
+            $this->query = "SELECT * FROM `student` WHERE `StudentID` = ".$id;
         }else {
-            $this->query = "SELECT * FROM teacher WHERE TeacherID = ".$id;
+            $this->query = "SELECT * FROM `teacher` WHERE `TeacherID` = ".$id;
         }
 
         return $this->connection->query($this->query);
@@ -59,26 +59,26 @@ class Connection {
 
     //Get class details from id
     public function getClassByID(int $id) {
-        $this->query = "SELECT * FROM class WHERE ClassID = ".$id;
+        $this->query = "SELECT * FROM `class` WHERE `ClassID` = ".$id;
         return $this->connection->query($this->query);
     }
 
     //Get student details within a class
     public function getStudentsByID(int $id) {
-        $this->query = "SELECT * FROM Student WHERE ClassID = ".$id;
+        $this->query = "SELECT * FROM `Student` WHERE `ClassID` = ".$id;
         return $this->connection->query($this->query);
     }
 
     //Get class details from code
     public function getClassByCode(string $code) {
-        $this->query = "SELECT * FROM class WHERE ClassCode = '".$code."'";
+        $this->query = "SELECT * FROM `class` WHERE `ClassCode` = '".$code."'";
         return $this->connection->query($this->query);
     }
 
     //Create an account of certain type for a user and then return the account details
     public function createAccount($email, $username, $password, $accountType) {
         $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
-        $query = "INSERT INTO ".$accountType."(Email, Username, Password) VALUES('".$email."', '".$email."', '".$hashedPassword."')";
+        $query = "INSERT INTO `".$accountType."`(Email, Username, Password) VALUES('".$email."', '".$email."', '".$hashedPassword."')";
 
         $this->connection->query($query);
 
@@ -87,18 +87,18 @@ class Connection {
     }
 
     public function getAssignmentsByClassID($id) {
-        $this->query = "SELECT AssignmentID, Date, topic.TopicName FROM assignment INNER JOIN topic on assignment.TopicID = topic.TopicID WHERE ClassID = ".$id." AND Date > CURRENT_DATE()";
+        $this->query = "SELECT `AssignmentID`, `Date`, `topic.TopicName` FROM `assignment` INNER JOIN `topic` on `assignment.TopicID` = `topic.TopicID` WHERE `ClassID` = ".$id." AND `Date` > CURRENT_DATE()";
         //echo $this->query;
         return $this->connection->query($this->query);
     }
 
     public function getClassesByTeacherID($id) {
-        $this->query = "SELECT * FROM Class WHERE TeacherID = ".$id;
+        $this->query = "SELECT * FROM `Class` WHERE `TeacherID` = ".$id;
         return $this->connection->query($this->query);
     }
 
     public function getClassByName($name) {
-        $this->query = "SELECT * FROM Class WHERE 'Class Name' = '".$name."'";
+        $this->query = "SELECT * FROM `Class` WHERE `Class Name` = '".$name."'";
         return $this->connection->query($this->query);
     }
 
