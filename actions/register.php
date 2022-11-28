44d73083a17e16fecdd087902e453f429a49a58f
Adam Steele <96735995+adamlsteele@users.noreply.github.com>
2022-11-28 12:51:30 +0000

    Fixed confirmation password logic

--- actions/register.php
+++ actions/register.php
@@ -15,7 +15,7 @@ if($_SERVER['REQUEST_METHOD'] != "POST") {
 }
 
 //Validation for entered passwords
-if($password != $confirmPassword) {
+if($password !== $confirmPassword) {
     header("Location: /register.php?error=Passwords entered do not match");
 }
 
